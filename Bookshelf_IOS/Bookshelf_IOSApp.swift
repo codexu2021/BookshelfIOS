ab28aaf1d633876cf78aab7d56442c69cb77157c
codexu2021 <82790256+codexu2021@users.noreply.github.com>
2021-06-14 20:09:42 +0900

    Initial Commit

---
+++ Bookshelf_IOS/Bookshelf_IOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  Bookshelf_IOSApp.swift
+//  Bookshelf_IOS
+//
+//  Created by 金岡雄一郎 on 2021/06/14.
+//
+
+import SwiftUI
+
+@main
+struct Bookshelf_IOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
